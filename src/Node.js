84e2e9a7c35194199ee00146b57c04620979da73
Bird <birdhello@sina.com>
2020-10-16 11:12:56 +0800

    [refine] add webaudio、fontface、accelerometer module (#3)

--- src/Node.js
+++ src/Node.js
@@ -1,44 +1,5 @@
 import EventTarget from './EventTarget'
 import Event from "./Event";
-import FILE_CACHE from "./util/FileCache"
-
-const _BASE64_NAME = "data:application/javascript;base64,";
-const _URI_NAME = "data:application/javascript,";
-let _getPathFromBase64String = function (src) {
-    if (src === null || src === undefined) {
-        return src;
-    }
-    if (src.startsWith(_BASE64_NAME)) {
-        let content = src.substring(_BASE64_NAME.length);
-        let source = window.atob(content);
-        let len = source.length;
-        if (len > 0) {
-            return _getDiskPathFromArrayBuffer(source, len);
-        } else {
-            return src;
-        }
-    } else if (src.startsWith(_URI_NAME)) {
-        let content = src.substring(_URI_NAME.length);
-        let source = decodeURIComponent(content);
-        let len = source.length;
-        if (len > 0) {
-            return _getDiskPathFromArrayBuffer(source, len);
-        } else {
-            return src;
-        }
-    } else {
-        return src;
-    }
-};
-
-function _getDiskPathFromArrayBuffer(source, len) {
-    var arrayBuffer = new ArrayBuffer(len);
-    var uint8Array = new Uint8Array(arrayBuffer);
-    for (let i = 0; i < len; i++) {
-        uint8Array[i] = source.charCodeAt(i);
-    }
-    return FILE_CACHE.getCache(arrayBuffer);
-}
 
 export default class Node extends EventTarget {
     childNodes = [];
@@ -53,11 +14,9 @@ export default class Node extends EventTarget {
     appendChild(node) {
         this.childNodes.push(node);
         node.parentNode = this;
-        if (node.nodeName === "SCRIPT") {
-            let src = _getPathFromBase64String(node.src);
-            console.log(src);
-            require(src);
-            node.dispatchEvent(new Event('load'));
+        let nodeName = node.nodeName;
+        if (nodeName === "SCRIPT" || nodeName === "STYLE") {
+            node.dispatchEvent(new Event("append"));
         }
         return node;
     }
