9c6c4c03ed4b3b0dae3e2042eed093498819f98d
Bird <birdhello@sina.com>
2020-12-09 11:58:28 +0800

    [refine] optimize audio module(src、load、play) (#16)

--- src/HTMLAudioElement.js
+++ src/HTMLAudioElement.js
@@ -7,6 +7,14 @@ const _INITIALIZING = 0;
 const _PLAYING = 1;
 const _PAUSE = 2;
 
+const _audio_valid_id = function(audioID) {
+    return typeof audioID === "number";
+};
+
+const _audio_valid_src = function (src) {
+    return typeof src === "string" && src !== "";
+};
+
 export default class HTMLAudioElement extends HTMLMediaElement {
     constructor(url) {
         super(url, 'AUDIO');
@@ -14,7 +22,7 @@ export default class HTMLAudioElement extends HTMLMediaElement {
 
     get currentTime() {
         let audioID = _weakMap.get(this).audioID;
-        if (typeof audioID === "number") {
+        if (_audio_valid_id(audioID)) {
             return jsb.AudioEngine.getCurrentTime(audioID);
         } else {
             return super.currentTime;
@@ -23,7 +31,7 @@ export default class HTMLAudioElement extends HTMLMediaElement {
 
     set currentTime(value) {
         let audioID = _weakMap.get(this).audioID;
-        if (typeof audioID === "number") {
+        if (_audio_valid_id(audioID)) {
             jsb.AudioEngine.setCurrentTime(audioID, value);
         }
         super.currentTime = value;
@@ -31,7 +39,7 @@ export default class HTMLAudioElement extends HTMLMediaElement {
 
     get duration() {
         let audioID = _weakMap.get(this).audioID;
-        if (typeof audioID === "number") {
+        if (_audio_valid_id(audioID)) {
             return jsb.AudioEngine.getDuration(audioID);
         } else {
             return super.duration;
@@ -40,7 +48,7 @@ export default class HTMLAudioElement extends HTMLMediaElement {
 
     get loop() {
         let audioID = _weakMap.get(this).audioID;
-        if (typeof audioID === "number") {
+        if (_audio_valid_id(audioID)) {
             return jsb.AudioEngine.isLoop(audioID);
         } else {
             return super.loop;
@@ -49,7 +57,7 @@ export default class HTMLAudioElement extends HTMLMediaElement {
 
     set loop(value) {
         let audioID = _weakMap.get(this).audioID;
-        if (typeof audioID === "number") {
+        if (_audio_valid_id(audioID)) {
             jsb.AudioEngine.setLoop(audioID, value);
         }
         super.loop = value;
@@ -57,7 +65,7 @@ export default class HTMLAudioElement extends HTMLMediaElement {
 
     get volume() {
         let audioID = _weakMap.get(this).audioID;
-        if (typeof audioID === "number") {
+        if (_audio_valid_id(audioID)) {
             return jsb.AudioEngine.getVolume(audioID);
         } else {
             return super.volume;
@@ -66,7 +74,7 @@ export default class HTMLAudioElement extends HTMLMediaElement {
 
     set volume(value) {
         let audioID = _weakMap.get(this).audioID;
-        if (typeof audioID === "number") {
+        if (_audio_valid_id(audioID)) {
             jsb.AudioEngine.setVolume(audioID, value);
         }
         super.volume = value;
@@ -83,24 +91,66 @@ export default class HTMLAudioElement extends HTMLMediaElement {
         return ''
     }
 
+    get src() {
+        return super.src;
+    }
+
+    set src(value) {
+        let privateThis = _weakMap.get(this);
+        // 停止有效的音频
+        let audioID = privateThis.audioID;
+        if (_audio_valid_id(audioID)) {
+            jsb.AudioEngine.stop(audioID);
+            privateThis.audioID = null;
+        }
+        super.src = value;
+        if(_audio_valid_src(value)) {
+            if (this.autoplay || this.preload === "auto") {
+                this.load();
+            }
+        } else {
+            console.error("invalid src: ", value);
+            this.dispatchEvent(new Event("error"));
+        }
+    }
+
     load() {
-        if (this.src !== "") {
+        let privateThis = _weakMap.get(this);
+        // 停止有效的音频
+        let audioID = privateThis.audioID;
+        if (_audio_valid_id(audioID)) {
+            jsb.AudioEngine.stop(audioID);
+            privateThis.audioID = null;
+        }
+
+        let src = this.src;
+        if (_audio_valid_src(src)) {
             this.dispatchEvent({type: "loadstart"});
             let self = this;
             jsb.AudioEngine.preload(this.src, function () {
+                // 已经预加载过的，不是异步回调，所以加上 setTimeout
                 setTimeout(function () {
-                    self.dispatchEvent(new Event("loadedmetadata"));
-                    self.dispatchEvent(new Event("loadeddata"));
-                    self.dispatchEvent(new Event("canplay"));
-                    self.dispatchEvent(new Event("canplaythrough"));
+                    // 异步过程，src 可能发生了变化
+                    if (self.src === src) {
+                        if (self.autoplay) {
+                            self.play();
+                        }
+                        self.dispatchEvent(new Event("loadedmetadata"));
+                        self.dispatchEvent(new Event("loadeddata"));
+                        self.dispatchEvent(new Event("canplay"));
+                        self.dispatchEvent(new Event("canplaythrough"));
+                    }
                 });
             });
+        } else {
+            console.error("invalid src: ", src);
+            this.dispatchEvent(new Event("error"));
         }
     }
 
     pause() {
         let audioID = _weakMap.get(this).audioID;
-        if (audioID !== undefined) {
+        if (_audio_valid_id(audioID)) {
             if (jsb.AudioEngine.getState(audioID) !== _PAUSE) {
                 jsb.AudioEngine.pause(audioID);
                 this.dispatchEvent(new Event("pause"));
@@ -113,14 +163,14 @@ export default class HTMLAudioElement extends HTMLMediaElement {
     }
 
     play() {
-        if (this.src === "") {
+        if (!_audio_valid_src(this.src)) {
             this.dispatchEvent({type: "emptied"});
             console.error("Audio play: please define src before play");
             return;
         }
 
         let audioID = _weakMap.get(this).audioID;
-        if (audioID !== undefined) {
+        if (_audio_valid_id(audioID)) {
             let state = jsb.AudioEngine.getState(audioID);
             switch (state) {
                 case _PAUSE: {
@@ -150,7 +200,10 @@ export default class HTMLAudioElement extends HTMLMediaElement {
             this.dispatchEvent(new Event("error"));
             return;
         }
-        jsb.AudioEngine.setCurrentTime(audioID, this.currentTime);
+        let currentTime = this.currentTime;
+        if (typeof currentTime === "number" && currentTime > 0) {
+            jsb.AudioEngine.setCurrentTime(audioID, currentTime);
+        }
 
         this.dispatchEvent(new Event("play"));
         jsb.AudioEngine.setFinishCallback(audioID, function () {
