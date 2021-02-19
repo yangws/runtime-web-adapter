9462047f2a3c700b8246a09a4215197cb9668648
Unknown <a563294062@qq.com>
2021-02-19 18:13:24 +0800

    [feature] add laya engine adapter

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -96,8 +96,39 @@ gulp.task("cocos-play.min.js", () => {
         .pipe(gulp.dest('./dist/platforms/cocos-play/'));
 });
 
+gulp.task("cocos-laya.js", () => {
+    return browserify('./laya/index.js')
+        .transform(babelify, {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+            comments: false
+        })
+        .bundle()
+        .pipe(source('laya-adapter.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task("cocos-laya.min.js", () => {
+    return browserify('./laya/index.js')
+        .transform(babelify, {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+            comments: false
+        })
+        .bundle()
+        .pipe(source('laya-adapter.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'));
+});
+
 gulp.task("web", gulp.series(["web.js", "web.min.js"]));
 gulp.task("cocos-runtime", gulp.series(["cocos-runtime.js", "cocos-runtime.min.js"]));
 gulp.task("cocos-play", gulp.series(["cocos-play.js", "cocos-play.min.js"]));
 gulp.task("ral", gulp.series(["cocos-runtime", "cocos-play"]));
 gulp.task('default', gulp.series(["web", "ral"]));
+gulp.task('cocos-laya', gulp.series(["cocos-laya.js", "cocos-laya.min.js"]));
---
+++ laya/index.js
@@ -0,0 +1,48 @@
+// 屏幕适配
+let _config;
+Object.defineProperty(window, "Config", {
+    get() {
+        return _config;
+    },
+    set(value) {
+        _config = value;
+        let _useRetinalCanvas = true;
+        Object.defineProperty(_config, "useRetinalCanvas", {
+            get() {
+                return _useRetinalCanvas;
+            },
+            set(value) {
+                _useRetinalCanvas = true;
+            }
+        });
+    }
+});
+
+//iOS版本字体重叠bug修复
+let res = loadRuntime().getSystemInfoSync();
+if (res && res.system.toLowerCase() !== 'ios 10.1.1') {
+    let _Laya;
+    Object.defineProperty(window, "Laya", {
+        get() {
+            return _Laya;
+        },
+        set(value) {
+            _Laya = value;
+            if (_Laya.Browser && !_Laya.Browser.onMiniGame) {
+                let _onMiniGame = true;
+                Object.defineProperty(_Laya.Browser, "onMiniGame", {
+                    get() {
+                        return _onMiniGame;
+                    },
+                    set(value) {
+                        _onMiniGame = true;
+                    }
+                });
+            }
+        }
+    });
+}
+
+require('../ral/cocos-runtime/index.js');
+require("../web/window.js");
+
