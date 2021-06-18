ee184e2dd956d64b4f8acad487c569542bf7a2e2
chencong <chencong_0111@163.com>
2021-06-18 10:01:37 +0800

    [bug]1.Add HTMLCanvasElement.js in Construct3 directory

--- construct3/index.js
+++ construct3/index.js
@@ -1,6 +1,15 @@
 require('../ral/cocos-runtime/index.js');
 require("../web/window.js");
 
+window.HTMLCanvasElement = require("./HTMLCanvasElement.js")
+let oldCreateElement = document.createElement;
+document.createElement = function (name) {
+    if (name === "canvas") {
+        return new window.HTMLCanvasElement;
+    }
+    return oldCreateElement(name);
+}
+
 window.MessagePort = require("./MessagePort.js");
 window.MessageChannel = require("./MessageChannel.js");
 window.Worker = require("./Worker.js");
