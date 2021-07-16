dfcf07fe09bf2ccb37cc0169bd7368c636f21e01
lemon4ex <netchen@vip.qq.com>
2021-07-16 18:22:19 +0800

    [bug] fix the problem that the accelerometer is abnormal when running v1 (#69)
    * [bug] fix the problem that the accelerometer is abnormal when running v1
    
    * update code
    
    * update code

--- ral/kwai-mini-game/device/accelerometer.js
+++ ral/kwai-mini-game/device/accelerometer.js
@@ -6,15 +6,63 @@ ral.device = ral.device || {};
 
 if (_rt.offAccelerometerChange) {
     // runtime v2
-    ral.onAccelerometerChange = _rt.onAccelerometerChange.bind(_rt);
-    ral.offAccelerometerChange = _rt.offAccelerometerChange.bind(_rt);
+    // 使用兼容模式运行v1游戏，此时游戏会加载 rt_compatibility.js
+    if (_rt._compatibleMode === 1) {
+        let _systemInfo = _rt.getSystemInfoSync();
+        let _isAndroid = _systemInfo.platform.toLowerCase() === "android";
+        let _compatibleAccelerometerChange = function (e) {
+            if (_isAndroid) {
+                e.x /= -10;
+                e.y /= -10;
+                e.z /= -10;
+            } else {
+                e.x /= 10;
+                e.y /= 10;
+                e.z /= 10;
+            }
+            _listeners.forEach(function (listener) {
+                listener(e);
+            });
+        };
+
+        let _onAccelerometerChange = _rt.onAccelerometerChange.bind(_rt);
+        ral.onAccelerometerChange = function (listener) {
+            if (typeof listener === "function") {
+                let length = _listeners.length;
+                for (let index = 0; index < length; ++index) {
+                    if (listener === _listeners[index]) {
+                        return;
+                    }
+                }
+                _listeners.push(listener);
+                if (_listeners.length === 1) {
+                    _onAccelerometerChange(_compatibleAccelerometerChange);
+                }
+            }
+        };
+        let _offAccelerometerChange = _rt.offAccelerometerChange.bind(_rt);
+        ral.offAccelerometerChange = function (listener) {
+            let length = _listeners.length;
+            for (let index = 0; index < length; ++index) {
+                if (listener === _listeners[index]) {
+                    _listeners.splice(index, 1);
+                    if (_listeners.length === 0) {
+                        _offAccelerometerChange(_compatibleAccelerometerChange);
+                    }
+                    break;
+                }
+            }
+        };
+    } else {
+        ral.onAccelerometerChange = _rt.onAccelerometerChange.bind(_rt);
+        ral.offAccelerometerChange = _rt.offAccelerometerChange.bind(_rt);
+    }
     ral.stopAccelerometer = _rt.stopAccelerometer.bind(_rt);
     let _startAccelerometer = _rt.startAccelerometer.bind(_rt);
     ral.startAccelerometer = function (obj) {
         return _startAccelerometer(Object.assign({ type: "accelerationIncludingGravity" }, obj));
     };
 } else {
-
     // runtime v1
     ral.onAccelerometerChange = function (listener) {
         if (typeof listener === "function") {
