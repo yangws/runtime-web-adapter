74b535fa2b280256312792b15a9c1a886c006922
cc <chencong_0111@163.com>
2021-07-20 14:57:09 +0800

    [bug] fix the bug that the return of currentTime is string on oppo platform

--- ral/oppo-mini-game/media/audio.js
+++ ral/oppo-mini-game/media/audio.js
@@ -3,9 +3,21 @@ import _UTIL from "../../util"
 
 _UTIL.exportTo("AudioEngine", qg, ral);
 _UTIL.exportTo("createInnerAudioContext", qg, ral, function () {
-    if (_rt.AudioEngine) {
+    if (qg.AudioEngine) {
         ral.createInnerAudioContext = function () {
             return _CREATE_INNER_AUDIO_CONTEXT(qg.AudioEngine);
         };
+    }
+}, function () {
+    let ctx = ral.createInnerAudioContext();
+    let prototype = ctx.__proto__.constructor.prototype;
+    let desc = Object.getOwnPropertyDescriptor(prototype, "currentTime");
+    if (desc) {
+        let get = desc.get;
+        Object.defineProperty(prototype, "currentTime", {
+            get() {
+                return parseFloat(get.call(this));
+            }
+        });
     }
 });
