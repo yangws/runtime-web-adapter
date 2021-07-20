d1382b14e6a1ecd24b9a901d5ae1928db97c815e
CC9701 <73738313+CC9701@users.noreply.github.com>
2021-07-20 15:15:58 +0800

    [bug] fix the bug that the return of currentTime is string on oppo platform (#71)

--- ral/vivo-mini-game/media/audio.js
+++ ral/vivo-mini-game/media/audio.js
@@ -3,7 +3,7 @@ import _UTIL from "../../util"
 
 _UTIL.exportTo("AudioEngine", qg, ral);
 _UTIL.exportTo("createInnerAudioContext", qg, ral, function () {
-    if (_rt.AudioEngine) {
+    if (qg.AudioEngine) {
         ral.createInnerAudioContext = function () {
             return _CREATE_INNER_AUDIO_CONTEXT(qg.AudioEngine);
         };
