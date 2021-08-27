a2bdaf915c2a069620a500cf263451965922e1a1
cc <chencong_0111@163.com>
2021-08-27 11:35:57 +0800

    [bug] fix touch position error find in some notch screen on vivo platform

--- ral/vivo-mini-game/base/system-info.js
+++ ral/vivo-mini-game/base/system-info.js
@@ -1,10 +1,35 @@
 import _UTIL from "../../util"
 
 _UTIL.exportTo("env", qg, ral);
-_UTIL.exportTo("getSystemInfo", qg, ral);
+/**
+ * 处理 vivo 平台在部分刘海屏机型, touch事件发生偏移问题.
+ * 当前 qg.getSystemInfo/qg.getSystemInfoSync 返回的 screenHeight, windowHeight, safeArea.height 返回的值会包括刘海屏高度, 与实际显示区域screenHeight不符.
+ * 平台提供的 window.innerHeight 才是正确的 screenHeight
+ */
+qg._vivoInnerHeight = window.innerHeight;
+qg._vivoInnerWidth = window.innerWidth;
+
+const _getSystemInfo = qg.getSystemInfo;
+ral.getSystemInfo = function (object) {
+    let _callbacks = object;
+    if (_callbacks && typeof _callbacks.success === "function") {
+        let _success = _callbacks.success;
+        _callbacks.success = function (res) {
+            if (res) {
+                res.platform = "android";
+                res.screenHeight = qg._vivoInnerHeight;
+                res.screenWidth = qg._vivoInnerWidth;
+            }
+            _success(res);
+        }
+    }
+    return _getSystemInfo(_callbacks);
+};
 
 ral.getSystemInfoSync = function () {
     let systemInfo = qg.getSystemInfoSync();
     systemInfo.platform = "android";
+    systemInfo.screenHeight = qg._vivoInnerHeight;
+    systemInfo.screenWidth = qg._vivoInnerWidth;
     return systemInfo;
 };
--- ral/vivo-mini-game/interface/window.js
+++ ral/vivo-mini-game/interface/window.js
@@ -1,3 +1,5 @@
+let _callbackListenerArray = [];
+
 Object.defineProperty(window, "devicePixelRatio", {
     set(val) {
     },
@@ -5,3 +7,32 @@ Object.defineProperty(window, "devicePixelRatio", {
         return 1;
     }
 });
+
+/**
+ * 处理 vivo 平台在部分刘海屏机型, touch事件发生偏移问题。
+ * 当前 qg 空间下不包含 qg.onWindowResize 方法，故需要单独实现ral.onWindowResize
+ * 该修改./base/system-info.js 关联
+ */
+ral.onWindowResize = function (callBack) {
+    if (typeof callBack === "function") {
+        if (_callbackListenerArray.indexOf(callBack) !== -1) {
+            return;
+        }
+        _callbackListenerArray.push(callBack);
+    }
+};
+
+ral.offWindowResize = function (callBack) {
+    let index = _callbackListenerArray.indexOf(callBack);
+    if (index !== -1) {
+        _listeners.splice(index, 1);
+    }
+};
+
+window.resize = function (width, height) {
+    qg._vivoInnerWidth = width;
+    qg._vivoInnerHeight = height;
+    _callbackListenerArray.forEach(function (callback) {
+        callback(width, height);
+    })
+}
