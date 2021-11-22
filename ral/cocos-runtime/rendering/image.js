6a6c8aa8cdf9e8417cc68f9c9e4a803b4ad404d3
wen xing liao <39575994+xmliaowx@users.noreply.github.com>
2021-11-22 14:11:50 +0800

    [feature] support runtime v1.1 (#87)
    * [feature] support runtime v1.1
    
    * [adjust]

--- ral/cocos-runtime/rendering/image.js
+++ ral/cocos-runtime/rendering/image.js
@@ -3,7 +3,11 @@ import _FEATURE from "../../feature";
 
 let _rt = loadRuntime();
 
-_UTIL.exportTo("loadImageData", _rt, ral);
+_UTIL.exportTo("loadImageData", _rt, ral, function () {
+    if (window.jsb && typeof jsb.loadImage === "function") {
+        ral.loadImageData = jsb.loadImage;
+    }
+});
 _UTIL.exportTo("createImage", _rt, ral, function () {
     let featureValue = "unsupported";
     if (document && typeof document.createElement === "function") {
