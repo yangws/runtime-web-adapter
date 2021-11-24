7baf05ff96216dadd15e6d163a71a45a182c3fb1
wen xing liao <39575994+xmliaowx@users.noreply.github.com>
2021-11-24 15:56:34 +0800

    [bug] fixed loadFont error (#89)

--- ral/cocos-runtime/rendering/font.js
+++ ral/cocos-runtime/rendering/font.js
@@ -2,4 +2,8 @@ import _UTIL from "../../util"
 
 let _rt = loadRuntime();
 
-_UTIL.exportTo("loadFont", _rt, ral);
+_UTIL.exportTo("loadFont", _rt, ral, function () {
+    if (window.jsb && typeof jsb.loadFont === "function") {
+        ral.loadFont = jsb.loadFont;
+    }
+});
