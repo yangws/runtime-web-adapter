1f549a5acde822d4e77ddf363f121e6158db066c
cc <chencong_0111@163.com>
2021-12-06 09:52:52 +0800

    [bug] adjust the value of getSystemInfo.windowHeight in vivo platform and add some reference about  difference between screenHeight and windowHeight in runtime

--- ral/cocos-runtime/base/system-info.js
+++ ral/cocos-runtime/base/system-info.js
@@ -2,6 +2,17 @@ import _UTIL from "../../util"
 
 let _rt = loadRuntime();
 
+/**
+ * ral.env 属性说明
+ * USER_DATA_PATH  {string} 用户文件目录路径，开发者拥有对这个目录的读写权限。
+ */
 _UTIL.exportTo("env", _rt, ral);
 _UTIL.exportTo("getSystemInfo", _rt, ral);
+
+
+/**
+ * getSystemInfoSync 返回值的部分参数补充说明
+ * windowWidth(windowHeight) 为游戏的可使用窗口宽高。 当游戏运行在异形屏设备上，应用悬浮窗上，返回值与screenWidth(screenHeight) 不同
+ * 当游戏的显示方向发生变化 screenWidth、screenHeight、windowWidth、windowHeight，以及safeArea 中的属性也会变化。游戏横屏显示时 width 为长的一边， 竖屏显示时，width 为短的一边。
+ */
 _UTIL.exportTo("getSystemInfoSync", _rt, ral);
--- ral/vivo-mini-game/base/system-info.js
+++ ral/vivo-mini-game/base/system-info.js
@@ -4,11 +4,16 @@ _UTIL.exportTo("env", qg, ral);
 /**
  * 处理 vivo 平台在部分刘海屏机型, touch 事件发生偏移问题.
  * 当前 qg.getSystemInfo/qg.getSystemInfoSync 返回的 screenHeight, windowHeight, safeArea.height 返回的值会包括刘海屏高度, 与实际显示区域 screenHeight 不符.
- * 平台提供的 window.innerHeight 才是正确的 screenHeight
+ * 平台提供的 window.innerHeight 才是实际的游戏使用窗口的高度
  */
 qg._vivoInnerHeight = window.innerHeight;
 qg._vivoInnerWidth = window.innerWidth;
 
+
+/**
+ * (2021/12/3) Vivo 小游戏 API 文档未标明支持 pixelRatio，但可以获取到一个异常的值。这里将其实现为 1， 使得 pixelRatio 与 screenHeight(screenWidth)的乘积与设备的分辩率保持一致
+ * windowWidth(windowHeight) 为游戏的可使用窗口宽高。 当游戏运行在异形屏设备上，应用悬浮窗上，返回值与screenWidth(screenHeight) 不同
+ */
 const _getSystemInfo = qg.getSystemInfo;
 ral.getSystemInfo = function (object) {
     let _callbacks = object;
@@ -17,8 +22,9 @@ ral.getSystemInfo = function (object) {
         _callbacks.success = function (res) {
             if (res) {
                 res.platform = "android";
-                res.screenHeight = qg._vivoInnerHeight;
-                res.screenWidth = qg._vivoInnerWidth;
+                res.windowHeight = qg._vivoInnerHeight;
+                res.windowWidth = qg._vivoInnerWidth;
+                res.pixelRatio = 1;
             }
             _success(res);
         }
@@ -29,7 +35,8 @@ ral.getSystemInfo = function (object) {
 ral.getSystemInfoSync = function () {
     let systemInfo = qg.getSystemInfoSync();
     systemInfo.platform = "android";
-    systemInfo.screenHeight = qg._vivoInnerHeight;
-    systemInfo.screenWidth = qg._vivoInnerWidth;
+    systemInfo.windowHeight = qg._vivoInnerHeight;
+    systemInfo.windowWidth = qg._vivoInnerWidth;
+    systemInfo.pixelRatio = 1;
     return systemInfo;
 };
