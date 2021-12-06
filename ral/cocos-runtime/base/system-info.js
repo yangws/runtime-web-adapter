7a42f6046aea0af1ab2128656f86cc281bf31278
cc <chencong_0111@163.com>
2021-12-06 15:11:17 +0800

    [bug] adjust the reference before

--- ral/cocos-runtime/base/system-info.js
+++ ral/cocos-runtime/base/system-info.js
@@ -11,8 +11,12 @@ _UTIL.exportTo("getSystemInfo", _rt, ral);
 
 
 /**
- * getSystemInfoSync 返回值的部分参数补充说明
- * windowWidth(windowHeight) 为游戏的可使用窗口宽高。 当游戏运行在异形屏设备上，应用悬浮窗上，返回值与screenWidth(screenHeight) 不同
- * 当游戏的显示方向发生变化 screenWidth、screenHeight、windowWidth、windowHeight，以及safeArea 中的属性也会变化。游戏横屏显示时 width 为长的一边， 竖屏显示时，width 为短的一边。
+ * getSystemInfoSync 返回值的部分参数补充说明:
+ * pixelRatio 表示物理像素 / 逻辑像素。不同平台对该属性的支持程度不同，在 ral 层约定 pixelRatio 与 screenWidth(screenHeight) 的乘积为设备实际的分辩率(单位：物理像素)。
+ * screenWidth(screenHeight) 表示屏幕的宽高(单位:逻辑像素)。 screenWidth 在游戏横屏显示时表示屏幕较长一边，竖屏显示时表示较短的一边。
+ * windowWidth(windowHeight) 表示游戏的可使用窗口宽高(单位:逻辑像素)，当游戏设置 canvas 宽高超出该值会导致 canvas 显示不全。windowWidth
+ * 游戏横屏显示时表示窗口较长一边，竖屏显示时表示较短的一边。
+ *
+ * windowWidth(windowHeight) 与 screenWidth(screenHeight) 返回值不一定相同，例如当游戏运行在刘海屏设备上，或者以悬浮窗方式（显示区域只占屏幕一部分）运行。
  */
 _UTIL.exportTo("getSystemInfoSync", _rt, ral);
--- ral/vivo-mini-game/base/system-info.js
+++ ral/vivo-mini-game/base/system-info.js
@@ -1,19 +1,20 @@
 import _UTIL from "../../util"
 
 _UTIL.exportTo("env", qg, ral);
+
 /**
- * 处理 vivo 平台在部分刘海屏机型, touch 事件发生偏移问题.
- * 当前 qg.getSystemInfo/qg.getSystemInfoSync 返回的 screenHeight, windowHeight, safeArea.height 返回的值会包括刘海屏高度, 与实际显示区域 screenHeight 不符.
- * 平台提供的 window.innerHeight 才是实际的游戏使用窗口的高度
+ * 处理 vivo 平台在部分刘海屏机型, touch 事件发生偏移问题
+ * (2021/12/3) 根据 vivo 小游戏 API文档， Vivo 平台的 getSystemInfo 暂不支持 pixelRatio, windowHeight, windowWidth 属性(实际使用会获取到错误的值)。
+ *
+ * screenHeight 与 screenWidth 的返回值与设备实际的分辩率宽高数值相等，所以这里将 pixelRatio 返回值设为 1, 使得在 ral 层获取到的 pixelRatio 与
+ * screenHeight(screenWidth)的乘积与设备的分辩率保持一致。
+ *
+ * vivo 平台基于 web 标准实现了 window.innerWidth(window.innerHeight)，该属性表示表示浏览器viewport 的尺寸，高度上不包括状态栏（非游戏显示区域）
+ * 的高度，故基于这两个接口实现 windowHeight 与 windowWidth。
  */
 qg._vivoInnerHeight = window.innerHeight;
 qg._vivoInnerWidth = window.innerWidth;
 
-
-/**
- * (2021/12/3) Vivo 小游戏 API 文档未标明支持 pixelRatio，但可以获取到一个异常的值。这里将其实现为 1， 使得 pixelRatio 与 screenHeight(screenWidth)的乘积与设备的分辩率保持一致
- * windowWidth(windowHeight) 为游戏的可使用窗口宽高。 当游戏运行在异形屏设备上，应用悬浮窗上，返回值与screenWidth(screenHeight) 不同
- */
 const _getSystemInfo = qg.getSystemInfo;
 ral.getSystemInfo = function (object) {
     let _callbacks = object;
