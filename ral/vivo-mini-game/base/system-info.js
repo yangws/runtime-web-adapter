bcefba18ccfdd812ca177ebc50098f8cfddc5487
CC9701 <73738313+CC9701@users.noreply.github.com>
2021-12-20 09:42:58 +0800

    [bug] adjust the value of getSystemInfo.windowHeight in vivo platform… (#90)
    * [bug] adjust the value of getSystemInfo.windowHeight in vivo platform and add some reference about  difference between screenHeight and windowHeight in runtime
    
    * [bug] adjust the reference before

--- ral/vivo-mini-game/base/system-info.js
+++ ral/vivo-mini-game/base/system-info.js
@@ -1,10 +1,16 @@
 import _UTIL from "../../util"
 
 _UTIL.exportTo("env", qg, ral);
+
 /**
- * 处理 vivo 平台在部分刘海屏机型, touch 事件发生偏移问题.
- * 当前 qg.getSystemInfo/qg.getSystemInfoSync 返回的 screenHeight, windowHeight, safeArea.height 返回的值会包括刘海屏高度, 与实际显示区域 screenHeight 不符.
- * 平台提供的 window.innerHeight 才是正确的 screenHeight
+ * 处理 vivo 平台在部分刘海屏机型, touch 事件发生偏移问题
+ * (2021/12/3) 根据 vivo 小游戏 API文档， Vivo 平台的 getSystemInfo 暂不支持 pixelRatio, windowHeight, windowWidth 属性(实际使用会获取到错误的值)。
+ *
+ * screenHeight 与 screenWidth 的返回值与设备实际的分辩率宽高数值相等，所以这里将 pixelRatio 返回值设为 1, 使得在 ral 层获取到的 pixelRatio 与
+ * screenHeight(screenWidth)的乘积与设备的分辩率保持一致。
+ *
+ * vivo 平台基于 web 标准实现了 window.innerWidth(window.innerHeight)，该属性表示表示浏览器viewport 的尺寸，高度上不包括状态栏（非游戏显示区域）
+ * 的高度，故基于这两个接口实现 windowHeight 与 windowWidth。
  */
 qg._vivoInnerHeight = window.innerHeight;
 qg._vivoInnerWidth = window.innerWidth;
@@ -17,8 +23,9 @@ ral.getSystemInfo = function (object) {
         _callbacks.success = function (res) {
             if (res) {
                 res.platform = "android";
-                res.screenHeight = qg._vivoInnerHeight;
-                res.screenWidth = qg._vivoInnerWidth;
+                res.windowHeight = qg._vivoInnerHeight;
+                res.windowWidth = qg._vivoInnerWidth;
+                res.pixelRatio = 1;
             }
             _success(res);
         }
@@ -29,7 +36,8 @@ ral.getSystemInfo = function (object) {
 ral.getSystemInfoSync = function () {
     let systemInfo = qg.getSystemInfoSync();
     systemInfo.platform = "android";
-    systemInfo.screenHeight = qg._vivoInnerHeight;
-    systemInfo.screenWidth = qg._vivoInnerWidth;
+    systemInfo.windowHeight = qg._vivoInnerHeight;
+    systemInfo.windowWidth = qg._vivoInnerWidth;
+    systemInfo.pixelRatio = 1;
     return systemInfo;
 };
