fb96ff95ed3c907dfc4b3f246790868843b25271
cc <chencong_0111@163.com>
2022-01-20 11:43:05 +0800

    [bug] Adjust the commit before

--- ral/cocos-play/base/performance.js
+++ ral/cocos-play/base/performance.js
@@ -1,8 +1,4 @@
 import _UTIL from "../../util";
 let _rt = loadRuntime();
 
-_UTIL.exportTo("triggerGC", _rt, ral, function () {
-    ral.triggerGC = function () {
-        console.warn("The triggerGC is not supported on this platform");
-    }
-});
+_UTIL.exportTo("triggerGC", _rt, ral);
--- ral/huawei-quick-game/base/performance.js
+++ ral/huawei-quick-game/base/performance.js
@@ -1,7 +1,3 @@
 import _UTIL from "../../util";
 
-_UTIL.exportTo("triggerGC", hbs, ral, function () {
-    ral.triggerGC = function () {
-        console.warn("The triggerGC is not supported on this platform");
-    }
-});
+_UTIL.exportTo("triggerGC", hbs, ral);
