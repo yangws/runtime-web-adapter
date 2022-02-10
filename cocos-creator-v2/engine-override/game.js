ef472c6cc7940cac8b5e437cbb76ab218e2699ba
cc <chencong_0111@163.com>
2022-02-10 10:34:10 +0800

    [bug] adjust the commit before

---
+++ cocos-creator-v2/engine-override/game.js
@@ -0,0 +1,16 @@
+/**
+ * 适配 creator2.x 游戏在IOS 设备上无法正常运行的问题
+ * 原因: 在 IOS 设备上，引擎逻辑在调用 _initFrameSize 使用到 cc.game.frame.clientWidth (或 clientHeight)，
+ * frame 为 null 导致崩溃。
+ *
+ * 基于在新版插件的适配逻辑，引擎初始化 cc.game.frame 时值为 null
+ * 原因:在 jsb-compatible 的逻辑中，cc.game.frame 通过 DOM 中 node 的 parentNode 属性可以获取到 window.__canvas,
+ * 新版 web 层基于 web 标准实现，未添加父节点的 parentNode 将返回 null
+ */
+const _initRenderer = cc.game._initRenderer;
+cc.game._initRenderer = function () {
+    _initRenderer.call(cc.game);
+    if (!cc.game.frame) {
+        cc.game.frame = window.__canvas;
+    }
+}
--- cocos-creator-v2/engine-override/index.js
+++ cocos-creator-v2/engine-override/index.js
@@ -1,6 +1,6 @@
 // 修改引擎中实现的部分方法
 require("./asset-manager");
 require("./canvas-context2d");
+require("./game");
 require("./minigame-editbox-impl");
 require("./sys");
-require("./view");
