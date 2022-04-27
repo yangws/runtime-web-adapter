a2a0c0e74838175f2e1be6c62e898600ec7f5635
CC9701 <73738313+CC9701@users.noreply.github.com>
2022-04-27 11:36:00 +0800

    [feature] Add supplementary notes about innerAudioContext in ral api reference (#116)
    * [feature] Add supplementary notes about innerAudioContext in ral api reference
    
    * [feature] Enable oppo platform innerAudioContext.seek() can get effect before play
    
    * [feature] Enable vivo huawei cocosplay innerAudioContext.seek() before play
    
    * [feature] delete unused reference
    
    * [feature] Update reference

--- ral/oppo-mini-game/media/audio.js
+++ ral/oppo-mini-game/media/audio.js
@@ -20,4 +20,27 @@ _UTIL.exportTo("createInnerAudioContext", qg, ral, function () {
             }
         });
     }
+    /**
+     * 适配 OPPO 平台上的 innerAudioContext 在(set src 之后 、play 之前 || stop 状态下) seek 不会生效问题
+    */
+    const _weakMap = new WeakMap();
+    let oldSeek = prototype.seek;
+    let oldPlay = prototype.play;
+    prototype.seek = function (position) {
+        if (this.paused) {
+            // playing 状态下不用再使用 play 方法 seek 就会生效, 所以仅 stopped 和 paused 状态时保存
+            _weakMap.set(this, {
+                position: position
+            });
+        }
+        oldSeek.call(this, position);
+    }
+    prototype.play = function () {
+        oldPlay.call(this);
+        let privateThis = _weakMap.get(this);
+        if (privateThis && privateThis.position >= 0) {
+            oldSeek.call(this, privateThis.position);
+            privateThis.position = -1;
+        }
+    }
 });
