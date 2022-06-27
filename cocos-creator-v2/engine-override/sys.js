d6781d8fc0d84ed964646b82314e5cca8a7442a6
cc <chencong_0111@163.com>
2022-06-27 19:46:52 +0800

    [bug] support safeArea function

--- cocos-creator-v2/engine-override/sys.js
+++ cocos-creator-v2/engine-override/sys.js
@@ -47,3 +47,42 @@ sys.garbageCollect = ral.triggerGC;
 sys.restartVM = function () {
     console.error("The restartVM is not define!");
 }
+
+/**
+ * 手机屏幕安全区域功能，引擎代码中, 仅仅支持安卓、iOS 原生平台和微信小游戏平台。 这里参考了 2.4.5 版本 runtime 插件的适配逻辑
+ * sys.getSafeAreaRect 返回手机屏幕安全区域（设计分辨率为单位）
+ */
+let originSafeAreaRect = sys.getSafeAreaRect;
+let safeArea = ral.getSystemInfoSync().safeArea;
+if (safeArea == null) {
+    sys.getSafeAreaRect = function () {
+        console.warn("The cc.sys.getSafeAreaRect is not support on this platform!");
+        return originSafeAreaRect(arguments);
+    }
+} else {
+    sys.getSafeAreaRect = function () {
+        // 将 safeArea 的参数从设备的物理分辨率为单位的坐标系 转为 设计分辨率为单位的坐标系
+
+        // Get the real location on device.
+        let leftBottom = new cc.Vec2(safeArea.left, safeArea.bottom);
+        let rightTop = new cc.Vec2(safeArea.right, safeArea.top);
+
+        // convert leftBottom and rightTop point to UI coordinates
+        let view = cc.view;
+        let frameSize = view.getFrameSize();
+        let relatedPos = {
+            left: 0,
+            top: 0,
+            width: frameSize.width,
+            height: frameSize.height
+        };
+        view.convertToLocationInView(leftBottom.x, leftBottom.y, relatedPos, leftBottom);
+        view.convertToLocationInView(rightTop.x, rightTop.y, relatedPos, rightTop);
+
+        // convert leftBottom and rightTop point to design resolution coordinates
+        view._convertPointWithScale(leftBottom);
+        view._convertPointWithScale(rightTop);
+
+        return cc.rect(leftBottom.x, leftBottom.y, rightTop.x - leftBottom.x, rightTop.y - leftBottom.y);
+    }
+}
