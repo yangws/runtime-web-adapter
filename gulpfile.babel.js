6da6bdb5cc12e878d5b236ca7954881eec5478fe
dingfajing <316116652@qq.com>
2022-08-22 15:56:26 +0800

    [feature] add egret adapter (#121)
    Co-authored-by: dingfajing <dingfajing@gmail.com>

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -415,7 +415,34 @@ gulp.task("cocos-creator-v2.min.js", () => {
         .pipe(gulp.dest("./dist/"));
 });
 
+gulp.task("cocos-runtime-egret.js", () => {
+    return browserify({ entries: ["./ral/cocos-runtime/index.js", "./web/window.js", "./egret/index.js"] })
+        .transform(babelify, {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+            comments: false
+        })
+        .bundle()
+        .pipe(source("cocos-runtime-egret.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./dist/"));
+});
 
+gulp.task("cocos-runtime-egret.min.js", () => {
+    return browserify({ entries: ["./ral/cocos-runtime/index.js", "./web/window.js", "./egret/index.js"] })
+        .transform(babelify, {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+            comments: false
+        })
+        .bundle()
+        .pipe(source("cocos-runtime-egret.min.js"))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/"));
+});
 
 gulp.task("lib-web", gulp.series(["web.js", "web.min.js"]));
 gulp.task("lib-web-huawei", gulp.series(["huawei-quick-game.js", "huawei-quick-game.min.js", "web.js", "web.min.js", "lib-web-huawei.zip"]));
@@ -433,3 +460,4 @@ gulp.task("cocos-runtime-laya", gulp.series(["cocos-runtime-laya.js", "cocos-run
 gulp.task("cocos-runtime-pixiJS", gulp.series(["cocos-runtime-pixiJS.js", "cocos-runtime-pixiJS.min.js"]));
 gulp.task("cocos-runtime-phaser", gulp.series(["cocos-runtime-phaser.js", "cocos-runtime-phaser.min.js"]));
 gulp.task("cocos-runtime-construct3", gulp.series(["cocos-runtime-construct3.js", "cocos-runtime-construct3.min.js"]));
+gulp.task("cocos-runtime-egret", gulp.series(["cocos-runtime-egret.js", "cocos-runtime-egret.min.js"]));
